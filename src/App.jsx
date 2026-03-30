b5578072f2c963c916be8d434e792ad9bb064b6b
janardan04 <janardhanborse2003@gmail.com>
2026-03-30 22:33:46 +0530

    Fix scroll problem

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import ScrollToTop from './components/ScrollToTop';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import Home from './pages/Home';
@@ -12,6 +13,7 @@ import HallDetails from './pages/HallDetails';
 function App() {
   return (
     <Router>
+      <ScrollToTop />
       <div className="flex flex-col min-h-screen">
         <Navbar />
         <main className="flex-1 pt-[72px]">
